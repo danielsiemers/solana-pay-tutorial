3a62aaa789463e8dfa4fe0a0ff659f6ee7e41589
Daniel <daniel.siemers@gmail.com>
2022-05-31 23:49:50 +1200

    Loyalty Cards
    Create a token as a loyalty card that offers a discount after the customer makes 5 purchases

--- lib/addresses.ts
+++ lib/addresses.ts
@@ -4,3 +4,5 @@ import { PublicKey } from "@solana/web3.js"
 export const shopAddress = new PublicKey('65ZkyKAEcpVTus1mNEUafXVE69QxAafLATUSgecatvkt') 
 // this is the same for everyone!
 export const usdcAddress = new PublicKey('Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr')
+// This is your token/coupon address
+export const couponAddress = new PublicKey('3vRiEofPLeFB412jqkEotqPi1P2oiMvEHmX467zPyYyN')
