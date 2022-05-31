1505c0afe3e592ebb46eeeee7827843100c7dfe0
Daniel <daniel.siemers@gmail.com>
2022-05-31 21:40:37 +1200

    Standard checkout process
    Connect browser wallet to the app and transfer funds (SOL or USDC) from customer to merchant

---
+++ lib/adresses.ts
@@ -0,0 +1,6 @@
+import { PublicKey } from "@solana/web3.js"
+
+// Your shop wallet address
+export const shopAddress = new PublicKey('65ZkyKAEcpVTus1mNEUafXVE69QxAafLATUSgecatvkt') 
+// this is the same for everyone!
+export const usdcAddress = new PublicKey('Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr')
